eb4229ce50a870c308ef67c98c85b979e1ad655f
Alex Olsen <ao@aotech.local>
2023-02-22 20:27:29 -0800

    added routes for users

--- gif-t-api/server.js
+++ gif-t-api/server.js
@@ -11,8 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-//const uri =
- //   "mongodb+srv://fender2440:Z31dr1$0204@practicedb.n1bp0zj.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(uri, { useNewUrlParser: true });
 
@@ -20,6 +18,10 @@ const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('MongoDB database connection established successfully');
 });
+
+const usersRouter = require('./routes/users');
+
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
